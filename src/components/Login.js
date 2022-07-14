5c4c074ef46568928f4d046a1231c264827ff707
TranVanNghia <TranNghia170735@gmail.com>
2022-07-14 22:20:11 +0700

    [FE][04] Add Logos Images

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,10 @@ const Login = (props) => {
     return (
         <Container>
             <Content>
+                <CTA>
+                    <CTALogoOne src="/images/cta-logo-one.svg"/>
+                    {/* <SignUp>GET ALL THERE</SignUp> */}
+                </CTA>
                 <BgImage/>
             </Content>
         </Container>
@@ -45,6 +49,21 @@ const BgImage = styled.div`
     right: 0;
     left: 0;
     z-index:-1;
+`
+
+const CTA = styled.div`
+    max-width: 650px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;   
+`
+
+const CTALogoOne = styled.img`
+    margin-bottom: 12px;
+    max-width: 600px;
+    min-height: 1px;
+    display: block;
+    width: 100%;
 `
 
 export default Login;
